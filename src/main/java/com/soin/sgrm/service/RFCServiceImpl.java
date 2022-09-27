deffba4e008692eef4dca82e2fb47bd305721718
unknown <aalvarezn@soin.co.cr>
2022-09-27 12:25:00 -0600

    Arreglo incidencia ordenamiento RFCxdate

--- src/main/java/com/soin/sgrm/service/RFCServiceImpl.java
+++ src/main/java/com/soin/sgrm/service/RFCServiceImpl.java
@@ -13,6 +13,7 @@ import org.apache.log4j.Logger;
 import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Criterion;
 import org.hibernate.criterion.MatchMode;
+import org.hibernate.criterion.Order;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -87,7 +88,6 @@ public class RFCServiceImpl implements RFCService {
 
 		alias.put("status", "status");
 		alias.put("user", "user");
-		
 		String[] range = (dateRange != null) ? dateRange.split("-") : null;
 		if (range != null) {
 			if (range.length > 1) {
@@ -107,11 +107,9 @@ public class RFCServiceImpl implements RFCService {
 		Criterion qSrch = null;
 		if (sSearch != null && sSearch.length() > 0) {
 			qSrch = Restrictions.or(
-
 					Restrictions.like("numRequest", sSearch, MatchMode.ANYWHERE).ignoreCase(),
 					Restrictions.like("reasonChange", sSearch, MatchMode.ANYWHERE).ignoreCase(),
 					Restrictions.like("user.fullName", sSearch, MatchMode.ANYWHERE).ignoreCase()
-					
 					);
 		}
 		if (sStatus == 0) {
@@ -134,7 +132,8 @@ public class RFCServiceImpl implements RFCService {
 		 
 
 		List<String> fetchs = new ArrayList<String>();
-		return dao.findAll(sEcho, iDisplayStart, iDisplayLength, columns, qSrch, fetchs, alias);
+		
+		return dao.findAll(sEcho, iDisplayStart, iDisplayLength, columns, qSrch, fetchs, alias,1);
 	}
 
 	public String verifySecuence(String partCode) {
@@ -271,7 +270,7 @@ public class RFCServiceImpl implements RFCService {
 		fetchs.add("files");
 		fetchs.add("tracking");
 		fetchs.add("user");
-		return dao.findAll(sEcho, iDisplayStart, iDisplayLength, columns, qSrch, fetchs, alias);
+		return dao.findAll(sEcho, iDisplayStart, iDisplayLength, columns, qSrch, fetchs, alias,1);
 	}
 
 }
