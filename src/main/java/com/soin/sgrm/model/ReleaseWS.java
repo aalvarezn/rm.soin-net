ba1e758a93719edf2aa919841be3987f40ab55fe
unknown <aalvarezn@soin.co.cr>
2022-12-20 10:19:36 -0600

    se crea webService para ERP

---
+++ src/main/java/com/soin/sgrm/model/ReleaseWS.java
@@ -0,0 +1,114 @@
+package com.soin.sgrm.model;
+
+public class ReleaseWS {
+	String system;
+	String description;
+	String observations;
+	String requirement;
+	String requirementName;
+	String userId;
+	String  desc;
+	String objects;
+	String auto;
+	String solTecnic;
+	String soluFunc;
+	String consecNoInstala;
+	Integer prioridad;
+	Integer riesgo;
+	Integer impacto;
+	public String getSystem() {
+		return system;
+	}
+	public void setSystem(String system) {
+		this.system = system;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getObservations() {
+		return observations;
+	}
+	public void setObservations(String observations) {
+		this.observations = observations;
+	}
+	
+	public String getRequirement() {
+		return requirement;
+	}
+	public void setRequirement(String requirement) {
+		this.requirement = requirement;
+	}
+	public String getRequirementName() {
+		return requirementName;
+	}
+	public void setRequirementName(String requirementName) {
+		this.requirementName = requirementName;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	
+	
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	public String getObjects() {
+		return objects;
+	}
+	public void setObjects(String objects) {
+		this.objects = objects;
+	}
+	public String getAuto() {
+		return auto;
+	}
+	public void setAuto(String auto) {
+		this.auto = auto;
+	}
+	public String getSolTecnic() {
+		return solTecnic;
+	}
+	public void setSolTecnic(String solTecnic) {
+		this.solTecnic = solTecnic;
+	}
+	public String getSoluFunc() {
+		return soluFunc;
+	}
+	public void setSoluFunc(String soluFunc) {
+		this.soluFunc = soluFunc;
+	}
+	public String getConsecNoInstala() {
+		return consecNoInstala;
+	}
+	public void setConsecNoInstala(String consecNoInstala) {
+		this.consecNoInstala = consecNoInstala;
+	}
+	public Integer getPrioridad() {
+		return prioridad;
+	}
+	public void setPrioridad(Integer prioridad) {
+		this.prioridad = prioridad;
+	}
+	public Integer getRiesgo() {
+		return riesgo;
+	}
+	public void setRiesgo(Integer riesgo) {
+		this.riesgo = riesgo;
+	}
+	public Integer getImpacto() {
+		return impacto;
+	}
+	public void setImpacto(Integer impacto) {
+		this.impacto = impacto;
+	}
+	
+	
+}
